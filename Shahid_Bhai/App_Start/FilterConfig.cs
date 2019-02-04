8ce9ce05ebaa0fc974fdae9cc3921232a4a7f8fb
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2019-02-04 14:49:36 +0500

    Add project files.

---
+++ Shahid_Bhai/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Shahid_Bhai
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
