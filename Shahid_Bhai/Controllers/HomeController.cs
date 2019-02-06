e3c860901d3b299c61cfde7328704375bf6f87ad
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2019-02-06 12:20:22 +0500

    CrudeDropDown

--- Shahid_Bhai/Controllers/HomeController.cs
+++ Shahid_Bhai/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shahid_Bhai.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,83 @@ namespace Shahid_Bhai.Controllers
 {
     public class HomeController : Controller
     {
+        MondayDropEntities1 db = new MondayDropEntities1();
+
         public ActionResult Index()
         {
-            return View();
+            var list = db.Employees.ToList();
+            return View(list);
         }
-
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
 
+            BindDropDownFOrCategory();
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Create(Employee emp)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            
+            db.Employees.Add(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            BindDropDownFOrCategory();
+            Employee emp =db.Employees.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee emp , int id)
+        {
+            Employee emp1=db.Employees.SingleOrDefault(m => m.EmpId == id);
+            emp1.EmpId = emp.EmpId;
+            emp1.Name = emp.Name;
+            emp1.CNIC = emp.CNIC;
+            emp1.Phone = emp.Phone;
+            emp1.CategoryId = emp.CategoryId;
+            emp1.ReligionId = emp.ReligionId;
+            emp1.GendrId = emp.GendrId;
+             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+
+            Employee emp = db.Employees.Find(id);
+            if (emp == null)
+            {
+                HttpNotFound();
+            }
+
+            return View(emp);
+        }
+        public ActionResult Delete(int id)
+        {
+
+             Employee emp = db.Employees.Find(id);
+            db.Employees.Remove(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        private void BindDropDownFOrCategory()
+        {
+            var category = db.CategoryTbs.ToList();
+            List<SelectListItem> catList = new SelectList(category, "CategoryId", "Title").ToList();
+            ViewBag.CategoryBag = catList;
+
+            var gender = db.GendrTbs.ToList();
+            List<SelectListItem> GanderList = new SelectList(gender, "GendrId", "gType").ToList();
+            ViewBag.genderBag = GanderList;
+
+            var religion = db.Religions.ToList();
+            List<SelectListItem> religionList = new SelectList(religion, "ReligionId", "name").ToList();
+            ViewBag.ReligionBag = religionList;
+
+        } 
+
     }
 }
